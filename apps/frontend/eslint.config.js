ac37c27c42ec838516fdd19a2a8ce475763ddd76
Anouar Aissaoui <tobia@crossbone.cc>
2024-08-25 06:59:09 +0200

    refactor: replace astro by next

--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -1,9 +1,25 @@
 // @ts-check
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from "@eslint/compat";
+import baseConfig from "@posture/eslint-config/eslint.config";
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+const patchedNextConfig = fixupConfigRules([...compat.extends("next/core-web-vitals")]);
+
+const config = [
+  ...baseConfig,
+  ...patchedNextConfig,
+  { ignores: [".next/*"] },
+];
+
+export default config;
